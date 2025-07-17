3d7a04328e0494598254921fa79cfe8b2db0fab9
LukaOniani <lukaa.oniani2002@gmail.com>
2025-07-17 12:52:14 +0400

    refactor InventoryClient: remove unused import for ResponseStatus

--- src/main/java/com/techie/microservices/order/client/InventoryClient.java
+++ src/main/java/com/techie/microservices/order/client/InventoryClient.java
@@ -4,9 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
-@FeignClient(value = "inventory", url = "http://localhost:8082" )
+@FeignClient(value = "inventory", url = "http://localhost:8082")
 public interface InventoryClient {
 
   @RequestMapping(method = RequestMethod.GET, value = "api/v1/inventory")
