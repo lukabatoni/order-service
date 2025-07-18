64a262cdc255ccb6f4007438e51d787eb61838ce
LukaOniani <lukaa.oniani2002@gmail.com>
2025-07-18 15:44:26 +0400

    refactor OrderService: integrate InventoryClient to check product stock before placing an order

--- src/main/java/com/techie/microservices/order/service/OrderService.java
+++ src/main/java/com/techie/microservices/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.techie.microservices.order.service;
 
+import com.techie.microservices.order.client.InventoryClient;
 import com.techie.microservices.order.dto.OrderRequestDto;
 import com.techie.microservices.order.model.Order;
 import com.techie.microservices.order.repository.OrderRepository;
@@ -11,13 +12,21 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class OrderService {
   private final OrderRepository orderRepository;
+  private final InventoryClient inventoryClient;
 
   public void placeOrder(OrderRequestDto orderRequestDto) {
-    Order order = new Order();
-    order.setOrderNumber(UUID.randomUUID().toString());
-    order.setSkuCode(orderRequestDto.skuCode());
-    order.setPrice(orderRequestDto.price());
-    order.setQuantity(orderRequestDto.quantity());
-    orderRepository.save(order);
+    var isProductInStock = inventoryClient.isInStock(
+        orderRequestDto.skuCode(), orderRequestDto.quantity());
+    if (isProductInStock){
+      Order order = new Order();
+      order.setOrderNumber(UUID.randomUUID().toString());
+      order.setSkuCode(orderRequestDto.skuCode());
+      order.setPrice(orderRequestDto.price());
+      order.setQuantity(orderRequestDto.quantity());
+      orderRepository.save(order);
+    }else {
+      throw new RuntimeException("Product with skucode " + orderRequestDto.skuCode() + " is not in stock");
+    }
+
   }
 }
