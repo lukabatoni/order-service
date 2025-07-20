0ae7252afd3fc967391ebc718e327bddaa1b9a75
LukaOniani <lukaa.oniani2002@gmail.com>
2025-07-20 19:22:33 +0400

    refactor OrderController: change placeOrder method to return ResponseEntity with CREATED status

--- src/main/java/com/techie/microservices/order/controller/OrderController.java
+++ src/main/java/com/techie/microservices/order/controller/OrderController.java
@@ -4,10 +4,10 @@ import com.techie.microservices.order.dto.OrderRequestDto;
 import com.techie.microservices.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,8 +18,8 @@ public class OrderController {
   private final OrderService orderService;
 
   @PostMapping
-  @ResponseStatus(HttpStatus.CREATED)
-  public void placeOrder(@RequestBody OrderRequestDto orderRequestDto) {
+  public ResponseEntity<Void> placeOrder(@RequestBody OrderRequestDto orderRequestDto) {
     orderService.placeOrder(orderRequestDto);
+    return ResponseEntity.status(HttpStatus.CREATED).build();
   }
 }
